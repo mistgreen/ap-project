c021f791806768ecb6551d135575b35252826019
Marie Collinge <marie.collinge@ada.ac.uk>
2024-02-20 18:21:27 +0000

    refactor footer to remove dead links

--- littlepartypro/src/lpp-frontend/src/components/FooterComponent.vue
+++ littlepartypro/src/lpp-frontend/src/components/FooterComponent.vue
@@ -1,10 +1,4 @@
 <template>
-  <div class="px-2 flex justify-evenly">
-    <h1 @click="toggleModal()">About</h1>
-    <h1 @click="toggleModal()">Terms</h1>
-    <h1 @click="toggleModal()">Privacy</h1>
-    <h1 @click="toggleModal()">FAQs</h1>
-  </div>
   <div>
     <p class="text-center p-4">© 2024 Little Party Pro</p>
   </div>
